888b76596143d2c1115cedfac82bb2f44c9184f5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-04-05 09:00:59 +0900

    fix

--- job.sh
+++ job.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-/usr/bin/curl -s https://github.com/nostr-protocol/nips/commits/master.atom | \
-    /go/bin/feed2json | \
-    /usr/bin/jq -c '.items[]' | \
-    /go/bin/ocinosql-dedup -k id -hashkey | \
-    /go/bin/jsonargs /algia_post.sh "{{.title}} #nips_changes" "{{.url}}"
+/usr/bin/curl -s -H "User-Agent: Chrome" "https://www.reddit.com/r/golang.json" | \
+    /usr/bin/jq -c '.data.children[].data' | \
+    /go/bin/ocinosql-dedup -k id | \
+    /go/bin/jsonargs -f "/algia_post.sh" "{{.title}} #golang_news" "{{.url}}"
