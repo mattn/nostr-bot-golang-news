170afada11fd805950c894ab6e70aa0af1863094
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-04-09 22:07:34 +0900

    fix script

--- job.sh
+++ job.sh
@@ -5,4 +5,4 @@ set -e
 /usr/bin/curl -s -H "User-Agent: Chrome" "https://www.reddit.com/r/golang.json" | \
     /usr/bin/jq -c '.data.children[].data' | \
     /go/bin/ocinosql-dedup -k id | \
-    /go/bin/jsonargs -f /go/bin/algia "{{.title}} #golang_news" "{{.url}}"
+    /go/bin/jsonargs -f /go/bin/algia -V n "{{.title}} #golang_news" "{{.url}}"
